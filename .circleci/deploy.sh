1d9377d30904fe7513ca2282ae09b884e516e995
Vipin Kundal <vipinkundal15@gmail.com>
2020-07-11 11:31:23 +0530

    Update deploy.sh
    Deploy script update

--- .circleci/deploy.sh
+++ .circleci/deploy.sh
@@ -1,12 +1,10 @@
-version: 2.1
-# read https://circleci.com/docs/2.0/env-vars/ to hide env
-orbs:
-  aws-code-deploy: circleci/aws-code-deploy@0.0.11
+#!/bin/bash
 
-workflows:
-  deploy_application:
-    jobs:
-      - aws-code-deploy/deploy:
-          application-name: Omtbiz.in
-          service-role-arn: myDeploymentGroupRoleARN
-          bundle-key: myS3BucketKey
+echo "Start deploy"
+cd ~/Omtbiz.in
+git pull origin master
+npm i
+npm run build 
+#pm2 stop build/server
+#pm2 start build/server
+echo "Deploy end"
