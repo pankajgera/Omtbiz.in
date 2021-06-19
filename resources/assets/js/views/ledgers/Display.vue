66650f8ae6730eeb18edd09484dbe000220250fe
vipinkundal <vipinkundal15@gmail.com>
2021-06-19 10:59:52 +0530

    Added quantity to ledger list

--- resources/assets/js/views/ledgers/Display.vue
+++ resources/assets/js/views/ledgers/Display.vue
@@ -2,7 +2,7 @@
   <div class="main-content item-create">
     <div class="page-header">
       <h3 class="page-title">
-        {{ ledgerData ? ledgerData.account : '' }}
+        {{ ledgerData ? ledgerData.account : "" }}
       </h3>
       <ol class="breadcrumb">
         <li class="breadcrumb-item">
@@ -22,20 +22,17 @@
         <div class="card">
           <div class="card-body">
             <table-component
-                ref="table"
-                :data="displayArray"
-                :show-filter="false"
-                table-class="table display-ledger"
-              >
-                <table-column
-                  :label="$t('ledgers.date')"
-                  show="date"
-                >
-                  <template slot-scope="row">
-                    {{ getFormattedDate(row.date) }}
-                  </template>
-                </table-column>
-                <!-- <table-column
+              ref="table"
+              :data="displayArray"
+              :show-filter="false"
+              table-class="table display-ledger"
+            >
+              <table-column :label="$t('ledgers.date')" show="date">
+                <template slot-scope="row">
+                  {{ getFormattedDate(row.date) }}
+                </template>
+              </table-column>
+              <!-- <table-column
                   :label="$t('ledgers.particulars')"
                   show="particulars"
                 >
@@ -43,81 +40,113 @@
                     {{ row.particulars }}
                   </template>
                 </table-column> -->
-                <table-column
-                  :label="$t('ledgers.particulars')"
-                  show="particulars"
-                >
-                  <template slot-scope="row">
-                    <router-link :to="{path: `/vouchers/${row.id}/edit`}" class="dropdown-item">
-                      {{ row.particulars }}
-                      </router-link>
-                  </template>
-                </table-column>
-                <table-column
-                  :label="$t('ledgers.voucher_type')"
-                  show="voucher_type"
-                >
-                  <template slot-scope="row">
-                    {{ row.voucher_type }}
-                  </template>
-                </table-column>
-                <table-column
-                  :label="$t('ledgers.voucher_id')"
-                  show="id"
-                >
-                  <template slot-scope="row">
-                    {{ row.id }}
-                  </template>
-                </table-column>
-                <table-column
-                  :label="$t('ledgers.debit')"
-                  show="debit"
-                >
-                  <template slot-scope="row">
-                    ₹ {{ row.debit ? row.debit : '0.00' }}
-                  </template>
-                </table-column>
-                <table-column
-                  :label="$t('ledgers.credit')"
-                  show="credit"
-                >
-                  <template slot-scope="row">
-                    ₹ {{ row.credit ? row.credit : '0.00' }}
-                  </template>
-                </table-column>
-              </table-component>
+              <table-column
+                :label="$t('ledgers.particulars')"
+                show="particulars"
+              >
+                <template slot-scope="row">
+                  <router-link
+                    :to="{ path: `/vouchers/${row.id}/edit` }"
+                    class="dropdown-item"
+                  >
+                    {{ row.particulars }}
+                  </router-link>
+                </template>
+              </table-column>
+              <table-column
+                :label="$t('ledgers.voucher_type')"
+                show="voucher_type"
+              >
+                <template slot-scope="row">
+                  {{ row.voucher_type }}
+                </template>
+              </table-column>
+              <table-column :label="$t('ledgers.voucher_id')" show="id">
+                <template slot-scope="row">
+                  {{ row.id }}
+                </template>
+              </table-column>
+              <table-column
+                :label="$t('ledgers.inventory_item_quantity')"
+                show="quantity"
+              >
+                <template slot-scope="row">
+                  {{ row.invoice ? row.invoice.inventories[0].quantity : 0 }}
+                </template>
+              </table-column>
+              <table-column :label="$t('ledgers.debit')" show="debit">
+                <template slot-scope="row">
+                  ₹ {{ row.debit ? row.debit : "0.00" }}
+                </template>
+              </table-column>
+              <table-column :label="$t('ledgers.credit')" show="credit">
+                <template slot-scope="row">
+                  ₹ {{ row.credit ? row.credit : "0.00" }}
+                </template>
+              </table-column>
+            </table-component>
           </div>
         </div>
         <div class="row" style="float: right">
           <div class="col-sm-12" style="width: 500px">
-            <hr/>
+            <hr />
             <p class="row">
               <span>Opening Balance:</span>
               <span class="ml-60">
-                {{ masterData.type === 'Dr' && masterData.opening_balance ? ' ₹ ' + parseFloat(masterData.opening_balance).toFixed(2) + ' ' + masterData.type : ' ₹ 0.00'}}
+                {{
+                  masterData.type === "Dr" && masterData.opening_balance
+                    ? " ₹ " +
+                      parseFloat(masterData.opening_balance).toFixed(2) +
+                      " " +
+                      masterData.type
+                    : " ₹ 0.00"
+                }}
               </span>
               <span class="ml-60">
-                {{ masterData.type === 'Cr' && masterData.opening_balance ? ' ₹ ' + parseFloat(masterData.opening_balance).toFixed(2) + ' ' + masterData.type : ' ₹ 0.00'}}
+                {{
+                  masterData.type === "Cr" && masterData.opening_balance
+                    ? " ₹ " +
+                      parseFloat(masterData.opening_balance).toFixed(2) +
+                      " " +
+                      masterData.type
+                    : " ₹ 0.00"
+                }}
               </span>
             </p>
-            <hr/>
+            <hr />
             <p class="row">
               <span class="mr-30">Current Total:</span>
               <span class="ml-60">
-                {{ currentTotalDebit ? ' ₹ ' + parseFloat(currentTotalDebit).toFixed(2) + ' Dr' : ' ₹ 0.00'}}
+                {{
+                  currentTotalDebit
+                    ? " ₹ " + parseFloat(currentTotalDebit).toFixed(2) + " Dr"
+                    : " ₹ 0.00"
+                }}
               </span>
               <span class="ml-60">
-                {{ currentTotalCredit ? ' ₹ ' + parseFloat(currentTotalCredit).toFixed(2) + ' Cr' : ' ₹ 0.00'}}
+                {{
+                  currentTotalCredit
+                    ? " ₹ " + parseFloat(currentTotalCredit).toFixed(2) + " Cr"
+                    : " ₹ 0.00"
+                }}
               </span>
             </p>
-            <hr/>
+            <hr />
             <h6 class="row">
               <span class="mr-10">Closing Balance:</span>
               <span class="ml-60">
-                {{ ledgerData.type === 'Dr' && ledgerData ? ' ₹ ' + parseFloat(ledgerData.balance).toFixed(2) + ' Dr': ' ₹ 0.00'}}
+                {{
+                  ledgerData.type === "Dr" && ledgerData
+                    ? " ₹ " + parseFloat(ledgerData.balance).toFixed(2) + " Dr"
+                    : " ₹ 0.00"
+                }}
               </span>
               <span class="ml-60">
-                {{ ledgerData.type === 'Cr' && ledgerData ? ' ₹ ' + parseFloat(ledgerData.balance).toFixed(2) + ' Cr': ' ₹ 0.00'}}
+                {{
+                  ledgerData.type === "Cr" && ledgerData
+                    ? " ₹ " + parseFloat(ledgerData.balance).toFixed(2) + " Cr"
+                    : " ₹ 0.00"
+                }}
               </span>
             </h6>
           </div>
@@ -143,8 +172,8 @@
 <style>
 .table.display-ledger {
   background: #f4f4ff;
-  margin:30px 0px;
-  padding:30px;
+  margin: 30px 0px;
+  padding: 30px;
   top: 15px;
 }
 </style>
@@ -161,7 +190,7 @@ const {
 // Vue editable grid component and styles
 import VueEditableGrid from "../../components/grid-table/VueEditableGrid";
 import "../../components/grid-table/VueEditableGrid.css";
-import moment from 'moment'
+import moment from "moment";
 
 export default {
   mixins: {
@@ -175,8 +204,8 @@ export default {
       isLoading: false,
       title: "Display Account Ledger",
       displayArray: [],
-      ledgerData: '',
-      masterData: '',
+      ledgerData: "",
+      masterData: "",
       currentTotalCredit: 0,
       currentTotalDebit: 0,
     };
@@ -187,16 +216,16 @@ export default {
   methods: {
     ...mapActions("ledger", ["fetchLedgerDisplay"]),
     async loadEditData() {
-      let response = await this.fetchLedgerDisplay(this.$route.params.id)
-      this.displayArray = response.data.vouchers
-      this.ledgerData = response.data.ledger
-      this.masterData = response.data.account_master
-      this.currentTotalCredit = this.ledgerData.credit
-      this.currentTotalDebit = this.ledgerData.debit
+      let response = await this.fetchLedgerDisplay(this.$route.params.id);
+      this.displayArray = response.data.vouchers;
+      this.ledgerData = response.data.ledger;
+      this.masterData = response.data.account_master;
+      this.currentTotalCredit = this.ledgerData.credit;
+      this.currentTotalDebit = this.ledgerData.debit;
     },
     getFormattedDate(date) {
-      return moment(date).format('DD-MM-YYYY');
-    }
+      return moment(date).format("DD-MM-YYYY");
+    },
   },
 };
 </script>
