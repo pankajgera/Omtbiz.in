a56e730e1f9af6ec1aec7f992be838167847d27f
vipinkundal <vipinkundal15@gmail.com>
2021-11-26 12:27:15 +0530

    Change into count

--- resources/assets/js/views/dispatch/Create.vue
+++ resources/assets/js/views/dispatch/Create.vue
@@ -252,7 +252,7 @@ export default {
     },
     invoiceWithAmount ({ invoice_number, due_amount, master}) {
       let count = this.invoice.filter(i => i.account_master_id === master.id).length
-      return `${invoice_number} (₹ ${parseFloat(due_amount).toFixed(2)}) - (${master.name}) - ${count}`
+      return `${invoice_number} (₹ ${parseFloat(due_amount).toFixed(2)}) - (${master.name}) * ${count}`
     },
     loadInvoice() {
       this.invoice = []
