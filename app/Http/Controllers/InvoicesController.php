00b8c7946ce4b79b4afe153fde14f5b2d66d111b
vipin kundal <vipinkundal15@gmail.com>
2022-06-14 16:07:32 +0530

    status changed

--- app/Http/Controllers/InvoicesController.php
+++ app/Http/Controllers/InvoicesController.php
@@ -161,7 +161,7 @@ class InvoicesController extends Controller
             $dispatch->save();
 
             $invoice->update([
-                'paid_status' => 'TO BE DISPATCHED',
+                'paid_status' => 'TO_BE_DISPATCH',
             ]);
 
             //Now for each inventory item create journal entry
