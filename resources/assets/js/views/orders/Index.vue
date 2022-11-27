550c50bbc47f50768ac9fd6a193ba1bcc77ba725
vipinkundal <vipinkundal15@gmail.com>
2022-11-27 12:06:00 +0530

    Order vue file added

---
+++ resources/assets/js/views/orders/Index.vue
@@ -0,0 +1,551 @@
+<template>
+  <div class="items main-content">
+    <div class="page-header">
+      <Header :title="$tc('orders.order', 2)" :bread-crumb-links="breadCrumbLinks">
+        <div v-show="totalEstimates || filtersApplied" class="mr-4 mb-3 mb-sm-0">
+          <base-button
+            :outline="true"
+            :icon="filterIcon"
+            size="large"
+            color="theme"
+            right-icon
+            @click="toggleFilter"
+          >
+            {{ $t('general.filter') }}
+          </base-button>
+        </div>
+        <div>
+          <router-link slot="item-title" to="/orders/create">
+            <base-button
+              size="large"
+              icon="plus"
+              color="theme">
+              {{ $t('orders.new_order') }}
+            </base-button>
+          </router-link>
+        </div>
+      </Header>
+    </div>
+
+    <transition name="fade">
+      <div v-show="showFilters" class="filter-section">
+        <div class="filter-container">
+          <div class="filter-customer">
+            <label>{{ $tc('customers.customer',1) }} </label>
+            <base-customer-select
+              ref="customerSelect"
+              @select="onSelectCustomer"
+              @deselect="clearCustomerSearch"
+            />
+          </div>
+          <!-- <div class="filter-status">
+            <label>{{ $t('orders.status') }}</label>
+            <base-select
+              v-model.trim="filters.status"
+              :options="status"
+              :group-select="false"
+              :searchable="true"
+              :show-labels="false"
+              :placeholder="$t('general.select_a_status')"
+              group-values="options"
+              group-label="label"
+              track-by="name"
+              label="name"
+              @remove="clearStatusSearch()"
+            />
+          </div> -->
+          <div class="filter-date">
+            <div class="from pr-3">
+              <label>{{ $t('general.from') }}</label>
+              <base-date-picker
+                v-model="filters.from_date"
+                :calendar-button="true"
+                calendar-button-icon="calendar"
+              />
+            </div>
+            <div class="dashed" />
+            <div class="to pl-3">
+              <label>{{ $t('general.to') }}</label>
+              <base-date-picker
+                v-model="filters.to_date"
+                :calendar-button="true"
+                calendar-button-icon="calendar"
+              />
+            </div>
+          </div>
+          <div class="filter-order">
+            <label>{{ $t('orders.order_number') }}</label>
+            <base-input
+              v-model="filters.order_number"
+              icon="hashtag"/>
+          </div>
+        </div>
+        <label class="clear-filter" @click="clearFilter">{{ $t('general.clear_all') }}</label>
+      </div>
+    </transition>
+
+    <div v-cloak v-show="showEmptyScreen" class="col-xs-1 no-data-info" align="center">
+      <moon-walker-icon class="mt-5 mb-4"/>
+      <div class="row" align="center">
+        <label class="col title">{{ $t('orders.no_orders') }}</label>
+      </div>
+      <div class="row">
+        <label class="description col mt-1" align="center">{{ $t('orders.list_of_orders') }}</label>
+      </div>
+      <div class="btn-container">
+        <base-button
+          :outline="true"
+          color="theme"
+          class="mt-3"
+          size="large"
+          @click="$router.push('orders/create')"
+        >
+          {{ $t('orders.new_order') }}
+        </base-button>
+      </div>
+    </div>
+
+    <div v-show="!showEmptyScreen" class="table-container">
+      <div class="table-actions mt-5">
+        <p class="table-stats">{{ $t('general.showing') }}: <b>{{ orders.length }}</b> {{ $t('general.of') }} <b>{{ totalEstimates }}</b></p>
+
+        <!-- Tabs -->
+        <!-- <ul class="tabs">
+          <li class="tab" @click="getStatus('UNPAID')">
+            <a :class="['tab-link', {'a-active': filters.status.value === 'UNPAID'}]" href="#" >{{ $t('general.due') }}</a>
+          </li>
+          <li class="tab" @click="getStatus('DRAFT')">
+            <a :class="['tab-link', {'a-active': filters.status.value === 'DRAFT'}]" href="#">{{ $t('general.draft') }}</a>
+          </li>
+          <li class="tab" @click="getStatus('')">
+            <a :class="['tab-link', {'a-active': filters.status.value === '' || filters.status.value === null || filters.status.value !== 'DRAFT' && filters.status.value !== 'UNPAID'}]" href="#">{{ $t('general.all') }}</a>
+          </li>
+        </ul> -->
+        <transition name="fade">
+          <v-dropdown v-if="selectedEstimates.length" :show-arrow="false">
+            <span slot="activator" href="#" class="table-actions-button dropdown-toggle">
+              {{ $t('general.actions') }}
+            </span>
+            <v-dropdown-item>
+              <div class="dropdown-item" @click="removeMultipleEstimates">
+                <font-awesome-icon :icon="['fas', 'trash']" class="dropdown-item-icon" />
+                {{ $t('general.delete') }}
+              </div>
+            </v-dropdown-item>
+          </v-dropdown>
+        </transition>
+      </div>
+      <div class="custom-control custom-checkbox">
+        <input
+          id="select-all"
+          v-model="selectAllFieldStatus"
+          type="checkbox"
+          class="custom-control-input"
+          @change="selectAllEstimates"
+        >
+        <label v-show="!isRequestOngoing" for="select-all" class="custom-control-label selectall">
+          <span class="select-all-label">{{ $t('general.select_all') }} </span>
+        </label>
+      </div>
+
+      <table-component
+        ref="table"
+        :show-filter="false"
+        :data="fetchData"
+        table-class="table"
+      >
+        <table-column
+          :sortable="false"
+          :filterable="false"
+          cell-class="no-click"
+        >
+          <template slot-scope="row">
+            <div class="custom-control custom-checkbox">
+              <input
+                :id="row.id"
+                v-model="selectField"
+                :value="row.id"
+                type="checkbox"
+                class="custom-control-input"
+              >
+              <label :for="row.id" class="custom-control-label"/>
+            </div>
+          </template>
+        </table-column>
+        <table-column
+          :label="$t('orders.number')"
+          show="order_number"
+        >
+          <template slot-scope="row">
+            <router-link :to="{path: `orders/${row.id}/edit?d=true`}" class="dropdown-item">
+               {{ row.order_number }}
+              </router-link>
+          </template>
+        </table-column>
+        <table-column
+          :label="$t('orders.date')"
+          sort-as="order_date"
+          show="formattedEstimateDate"
+        />
+        <table-column
+          :label="$t('orders.name')"
+          width="20%"
+          show="master.name"
+        />
+        <table-column
+          :label="$t('orders.count')"
+          width="20%"
+          show="items.length"
+        />
+        <!-- <table-column
+          :label="$t('orders.status')"
+          sort-as="status"
+        >
+          <template slot-scope="row" >
+            <span> {{ $t('orders.status') }}</span>
+            <span :class="'inv-status-'+row.status.toLowerCase()">{{ (row.status != 'PARTIALLY_PAID')? row.status : row.status.replace('_', ' ') }}</span>
+          </template>
+        </table-column> -->
+        <!-- <table-column
+          :label="$t('orders.status')"
+          sort-as="status"
+        >
+          <template slot-scope="row">
+            <span>{{ $t('orders.status') }}</span>
+            <span :class="'inv-status-'+row.status.toLowerCase()">{{ (row.status != 'DISPATCHED') ? 'PAID' : row.status }}</span>
+          </template>
+        </table-column> -->
+        <table-column
+          :label="$t('orders.total')"
+          sort-as="total"
+        >
+          <template slot-scope="row">
+            <span>{{ $t('orders.amount') }}</span>
+             	₹ {{ (row.total).toFixed(2) }}
+          </template>
+        </table-column>
+        <table-column
+          :sortable="false"
+          :filterable="false"
+          cell-class="action-dropdown no-click"
+        >
+          <template slot-scope="row">
+            <span>{{ $t('orders.action') }}</span>
+            <v-dropdown>
+              <a slot="activator" href="#">
+                <dot-icon />
+              </a>
+              <v-dropdown-item>
+                <router-link :to="{path: `orders/${row.id}/edit`}" class="dropdown-item" v-if="role === 'admin'">
+                  <font-awesome-icon :icon="['fas', 'pencil-alt']" class="dropdown-item-icon"/>
+                  {{ $t('general.edit') }}
+                </router-link>
+                <router-link :to="{path: `orders/${row.id}/view`}" class="dropdown-item">
+                  <font-awesome-icon icon="eye" class="dropdown-item-icon" />
+                  {{ $t('orders.view') }}
+                </router-link>
+              </v-dropdown-item>
+              <v-dropdown-item v-if="row.status == 'DRAFT'">
+                <a class="dropdown-item" href="#/" @click="sendEstimate(row.id)" v-if="role === 'admin'">
+                  <font-awesome-icon icon="paper-plane" class="dropdown-item-icon" />
+                  {{ $t('orders.send_order') }}
+                </a>
+              </v-dropdown-item>
+              <v-dropdown-item>
+                <div class="dropdown-item" @click="removeEstimate(row.id)" v-if="role === 'admin'">
+                  <font-awesome-icon :icon="['fas', 'trash']" class="dropdown-item-icon" />
+                  {{ $t('general.delete') }}
+                </div>
+              </v-dropdown-item>
+            </v-dropdown>
+          </template>
+        </table-column>
+      </table-component>
+    </div>
+  </div>
+</template>
+
+<script>
+import { mapActions, mapGetters } from 'vuex'
+import MoonWalkerIcon from '../../../js/components/icon/MoonwalkerIcon'
+import moment from 'moment'
+import BaseButton from '../../components/base/BaseButton'
+
+export default {
+  components: {
+    'moon-walker-icon': MoonWalkerIcon,
+    BaseButton,
+  },
+  data () {
+    return {
+      showFilters: false,
+      breadCrumbLinks:[
+        {
+          url:'dashboard',
+          title:this.$t('general.home'),
+        },
+        {
+          url:'#',
+          title:this.$tc('orders.order', 2)
+        }
+      ],
+      //currency: null,
+      status: [
+        {
+          label: 'Status',
+          isDisable: true,
+          options: [
+            { name: 'DRAFT', value: 'DRAFT' },
+            { name: 'DUE', value: 'UNPAID' },
+            { name: 'SENT', value: 'SENT' },
+            { name: 'VIEWED', value: 'VIEWED' },
+            { name: 'OVERDUE', value: 'OVERDUE' },
+            { name: 'COMPLETED', value: 'COMPLETED' }
+          ]
+        },
+        {
+          label: 'Paid Status',
+          options: [
+            { name: 'UNPAID', value: 'UNPAID' },
+            { name: 'PAID', value: 'PAID' },
+            { name: 'PARTIALLY PAID', value: 'PARTIALLY_PAID' }
+          ]
+        }
+      ],
+      filtersApplied: false,
+      isRequestOngoing: true,
+      filters: {
+        order_number: '',
+        customer: '',
+        status: { name: 'DUE', value: 'UNPAID' },
+        from_date: '',
+        to_date: ''
+      },
+      role: this.$store.state.user.currentUser.role
+    }
+  },
+
+  computed: {
+    showEmptyScreen () {
+      return !this.totalEstimates && !this.isRequestOngoing && !this.filtersApplied
+    },
+    filterIcon () {
+      return (this.showFilters) ? 'times' : 'filter'
+    },
+    ...mapGetters('customer', [
+      'customers'
+    ]),
+    ...mapGetters('order', [
+      'selectedEstimates',
+      'totalEstimates',
+      'orders',
+      'selectAllField'
+    ]),
+    selectField: {
+      get: function () {
+        return this.selectedEstimates
+      },
+      set: function (val) {
+        this.selectEstimate(val)
+      }
+    },
+    selectAllFieldStatus: {
+      get: function () {
+        return this.selectAllField
+      },
+      set: function (val) {
+        this.setSelectAllState(val)
+      }
+    }
+  },
+  watch: {
+    filters: {
+      handler: 'setFilters',
+      deep: true
+    }
+  },
+  created () {
+    this.fetchCustomers()
+  },
+  destroyed () {
+    if (this.selectAllField) {
+      this.selectAllEstimates()
+    }
+  },
+  methods: {
+    ...mapActions('order', [
+      'fetchEstimates',
+      'getRecord',
+      'selectEstimate',
+      'resetSelectedEstimates',
+      'selectAllEstimates',
+      'deleteEstimate',
+      'deleteMultipleEstimates',
+      'sendEmail',
+      'setSelectAllState'
+    ]),
+    ...mapActions('customer', [
+      'fetchCustomers'
+    ]),
+    async sendEstimate (id) {
+      swal({
+        title: this.$t('general.are_you_sure'),
+        text: this.$t('orders.confirm_send'),
+        icon: '/assets/icon/paper-plane-solid.svg',
+        buttons: true,
+        dangerMode: true
+      }).then(async (value) => {
+        if (value) {
+          const data = {
+            id: id
+          }
+          let response = await this.sendEmail(data)
+          this.refreshTable()
+          if (response.data.success) {
+            window.toastr['success'](this.$tc('orders.send_order_successfully'))
+            return true
+          }
+          if (response.data.error === 'user_email_does_not_exist') {
+            window.toastr['error'](this.$tc('orders.user_email_does_not_exist'))
+            return false
+          }
+          window.toastr['error'](this.$tc('orders.something_went_wrong'))
+        }
+      })
+    },
+    refreshTable () {
+      this.$refs.table.refresh()
+    },
+    async fetchData ({ page, filter, sort }) {
+      let data = {
+        order_number: this.filters.order_number,
+        customer_id: this.filters.customer === '' ? this.filters.customer : this.filters.customer.id,
+        status: '',
+        from_date: this.filters.from_date === '' ? this.filters.from_date : moment(this.filters.from_date).format('DD/MM/YYYY'),
+        to_date: this.filters.to_date === '' ? this.filters.to_date : moment(this.filters.to_date).format('DD/MM/YYYY'),
+        orderByField: sort.fieldName || 'created_at',
+        orderBy: sort.order || 'desc',
+        page
+      }
+
+      this.isRequestOngoing = true
+      let response = await this.fetchEstimates(data)
+      this.isRequestOngoing = false
+
+      //this.currency = response.data.currency
+
+      return {
+        data: response.data.orders.data,
+        pagination: {
+          totalPages: response.data.orders.last_page,
+          currentPage: page,
+          count: response.data.orders.count
+        }
+      }
+    },
+    setFilters () {
+      if (this.timer) {
+          clearTimeout(this.timer);
+          this.timer = null;
+      }
+      this.timer = setTimeout(() => {
+				this.filtersApplied = true
+        this.resetSelectedEstimates()
+        this.refreshTable()
+			}, 1000);
+    },
+    clearFilter () {
+      if (this.filters.customer) {
+        this.$refs.customerSelect.$refs.baseSelect.removeElement(this.filters.customer)
+      }
+      this.filters = {
+        order_number: '',
+        customer: '',
+        status: '',
+        from_date: '',
+        to_date: ''
+      }
+
+      this.$nextTick(() => {
+        this.filtersApplied = false
+      })
+    },
+    toggleFilter () {
+      if (this.showFilters && this.filtersApplied) {
+        this.clearFilter()
+        this.refreshTable()
+      }
+
+      this.showFilters = !this.showFilters
+    },
+    onSelectCustomer (customer) {
+      this.filters.customer = customer
+    },
+    async removeEstimate (id) {
+      this.id = id
+      swal({
+        title: this.$t('general.are_you_sure'),
+        text: this.$tc('orders.confirm_delete'),
+        icon: '/assets/icon/trash-solid.svg',
+        buttons: true,
+        dangerMode: true
+      }).then(async (value) => {
+        if (value) {
+          let res = await this.deleteEstimate(this.id)
+
+          if (res.data.success) {
+            window.toastr['success'](this.$tc('orders.deleted_message'))
+            this.$refs.table.refresh()
+            return true
+          }
+
+          if (res.data.error === 'payment_attached') {
+            window.toastr['error'](this.$t('orders.payment_attached_message'), this.$t('general.action_failed'))
+            return true
+          }
+
+          window.toastr['error'](res.data.error)
+          return true
+        }
+
+        this.$refs.table.refresh()
+        this.filtersApplied = false
+        this.resetSelectedEstimates()
+      })
+    },
+    async removeMultipleEstimates () {
+      swal({
+        title: this.$t('general.are_you_sure'),
+        text: this.$tc('orders.confirm_delete', 2),
+        icon: '/assets/icon/trash-solid.svg',
+        buttons: true,
+        dangerMode: true
+      }).then(async (value) => {
+        if (value) {
+          let res = await this.deleteMultipleEstimates()
+          if (res.data.error === 'payment_attached') {
+            window.toastr['error'](this.$t('orders.payment_attached_message'), this.$t('general.action_failed'))
+            return true
+          }
+          if (res.data) {
+            this.$refs.table.refresh()
+            this.filtersApplied = false
+            this.resetSelectedEstimates()
+            window.toastr['success'](this.$tc('orders.deleted_message', 2))
+          } else if (res.data.error) {
+            window.toastr['error'](res.data.message)
+          }
+        }
+      })
+    },
+    async clearCustomerSearch (removedOption, id) {
+      this.filters.customer = ''
+      this.refreshTable()
+    },
+    async clearStatusSearch (removedOption, id) {
+      this.filters.status = ''
+      this.refreshTable()
+    }
+  }
+}
+</script>
---
+++ resources/assets/js/views/orders/View.vue
@@ -0,0 +1,261 @@
+<template>
+  <div v-if="order" class="main-content order-view-page">
+    <div class="page-header">
+      <h3 class="page-title"> {{ order.order_number }}</h3>
+      <div class="page-actions row">
+        <v-dropdown :close-on-select="false" align="left" class="filter-container">
+          <a slot="activator" href="#">
+            <base-button color="theme">
+              <font-awesome-icon icon="ellipsis-h" />
+            </base-button>
+          </a>
+          <v-dropdown-item>
+            <router-link :to="{path: `/orders/${$route.params.id}/edit`}" class="dropdown-item">
+              <font-awesome-icon :icon="['fas', 'pencil-alt']" class="dropdown-item-icon"/>
+              {{ $t('general.edit') }}
+            </router-link>
+            <div class="dropdown-item" @click="removeEstimate($route.params.id)">
+              <font-awesome-icon :icon="['fas', 'trash']" class="dropdown-item-icon" />
+              {{ $t('general.delete') }}
+            </div>
+          </v-dropdown-item>
+        </v-dropdown>
+      </div>
+    </div>
+    <div class="order-sidebar">
+      <base-loader v-if="isSearching" />
+      <div v-else class="side-header">
+        <base-input
+          v-model="searchData.searchText"
+          :placeholder="$t('general.search')"
+          input-class="inv-search"
+          icon="search"
+          type="text"
+          align-icon="right"
+          @input="onSearched()"
+        />
+        <div
+          class="btn-group ml-3"
+          role="group"
+          aria-label="First group"
+        >
+          <v-dropdown :close-on-select="false" align="left" class="filter-container">
+            <a slot="activator" href="#">
+              <base-button class="inv-button inv-filter-fields-btn" color="default" size="medium">
+                <font-awesome-icon icon="filter" />
+              </base-button>
+            </a>
+
+            <div class="filter-items">
+              <input
+                id="filter_order_date"
+                v-model="searchData.orderByField"
+                type="radio"
+                name="filter"
+                class="inv-radio"
+                value="order_date"
+                @change="onSearched"
+              >
+              <label class="inv-label" for="filter_order_date">{{ $t('reports.orders.order_date') }}</label>
+            </div>
+            <div class="filter-items">
+              <input
+                id="filter_due_date"
+                v-model="searchData.orderByField"
+                type="radio"
+                name="filter"
+                class="inv-radio"
+                value="expiry_date"
+                @change="onSearched"
+              >
+              <label class="inv-label" for="filter_due_date">{{ $t('orders.due_date') }}</label>
+            </div>
+            <div class="filter-items">
+              <input
+                id="filter_order_number"
+                v-model="searchData.orderByField"
+                type="radio"
+                name="filter"
+                class="inv-radio"
+                value="order_number"
+                @change="onSearched"
+              >
+              <label class="inv-label" for="filter_order_number">{{ $t('orders.order_number') }}</label>
+            </div>
+          </v-dropdown>
+          <base-button class="inv-button inv-filter-sorting-btn" color="default" size="medium" @click="sortData">
+            <font-awesome-icon v-if="getOrderBy" icon="sort-amount-up" />
+            <font-awesome-icon v-else icon="sort-amount-down" />
+          </base-button>
+        </div>
+      </div>
+      <div class="side-content">
+        <router-link
+          v-for="(order,index) in orders"
+          :to="`/orders/${order.id}/view`"
+          :key="index"
+          class="side-order"
+        >
+          <div class="left">
+            <div class="inv-name">{{ order.user.name }}</div>
+            <div class="inv-number">{{ order.order_number }}</div>
+          </div>
+          <div class="right">
+            <div class="inv-amount" v-html="$utils.formatMoney(order.total, order.user.currency)" />
+            <div class="inv-date">{{ order.formattedEstimateDate }}</div>
+          </div>
+        </router-link>
+        <p v-if="!orders.length" class="no-result">
+          {{ $t('orders.no_matching_orders') }}
+        </p>
+      </div>
+    </div>
+    <div class="order-view-page-container">
+      <iframe :src="`${shareableLink}`" class="frame-style"/>
+    </div>
+  </div>
+</template>
+
+<script>
+import { mapActions } from 'vuex'
+const _ = require('lodash')
+
+export default {
+  data () {
+    return {
+      id: null,
+      count: null,
+      orders: [],
+      order: null,
+      currency: null,
+      searchData: {
+        orderBy: null,
+        orderByField: null,
+        searchText: null
+      },
+      isSendingEmail: false,
+      isRequestOnGoing: false,
+      isSearching: false
+    }
+  },
+  computed: {
+    getOrderBy () {
+      if (this.searchData.orderBy === 'asc' || this.searchData.orderBy == null) {
+        return true
+      }
+      return false
+    },
+
+    shareableLink () {
+      return `/orders/pdf/${this.order.unique_hash}`
+    }
+  },
+  watch: {
+    $route (to, from) {
+      this.loadEstimate()
+    }
+  },
+  created () {
+    this.loadEstimates()
+    this.loadEstimate()
+    this.onSearched = _.debounce(this.onSearched, 500)
+  },
+  methods: {
+    ...mapActions('order', [
+      'fetchEstimates',
+      'getRecord',
+      'searchEstimate',
+      'sendEmail',
+      'deleteEstimate',
+      'selectEstimate',
+      'fetchViewEstimate'
+    ]),
+    async loadEstimates () {
+      let response = await this.fetchEstimates()
+      if (response.data) {
+        this.orders = response.data.orders.data
+      }
+    },
+    async loadEstimate () {
+      let response = await this.fetchViewEstimate(this.$route.params.id)
+
+      if (response.data) {
+        this.order = response.data.order
+      }
+    },
+    async onSearched () {
+      let data = ''
+      if (this.searchData.searchText !== '' && this.searchData.searchText !== null && this.searchData.searchText !== undefined) {
+        data += `search=${this.searchData.searchText}&`
+      }
+
+      if (this.searchData.orderBy !== null && this.searchData.orderBy !== undefined) {
+        data += `orderBy=${this.searchData.orderBy}&`
+      }
+
+      if (this.searchData.orderByField !== null && this.searchData.orderByField !== undefined) {
+        data += `orderByField=${this.searchData.orderByField}`
+      }
+      this.isSearching = true
+      let response = await this.searchEstimate(data)
+      this.isSearching = false
+      if (response.data) {
+        this.orders = response.data.orders.data
+      }
+    },
+    sortData () {
+      if (this.searchData.orderBy === 'asc') {
+        this.searchData.orderBy = 'desc'
+        this.onSearched()
+        return true
+      }
+      this.searchData.orderBy = 'asc'
+      this.onSearched()
+      return true
+    },
+    async onSendEstimate (id) {
+      window.swal({
+        title: this.$t('general.are_you_sure'),
+        text: this.$t('orders.confirm_send_order'),
+        icon: '/assets/icon/paper-plane-solid.svg',
+        buttons: true,
+        dangerMode: true
+      }).then(async (value) => {
+        if (value) {
+          this.isSendingEmail = true
+          let response = await this.sendEmail({id: this.order.id})
+          this.isSendingEmail = false
+          if (response.data.success) {
+            window.toastr['success'](this.$tc('orders.send_order_successfully'))
+            return true
+          }
+          if (response.data.error === 'user_email_does_not_exist') {
+            window.toastr['error'](this.$tc('orders.user_email_does_not_exist'))
+            return true
+          }
+          window.toastr['error'](this.$tc('orders.something_went_wrong'))
+        }
+      })
+    },
+    async removeEstimate (id) {
+      window.swal({
+        title: 'Deleted',
+        text: 'you will not be able to recover this order!',
+        icon: '/assets/icon/trash-solid.svg',
+        buttons: true,
+        dangerMode: true
+      }).then(async (value) => {
+        if (value) {
+          let request = await this.deleteEstimate(id)
+          if (request.data.success) {
+            window.toastr['success'](this.$tc('orders.deleted_message', 1))
+            this.$router.push('/orders')
+          } else if (request.data.error) {
+            window.toastr['error'](request.data.message)
+          }
+        }
+      })
+    }
+  }
+}
+</script>
