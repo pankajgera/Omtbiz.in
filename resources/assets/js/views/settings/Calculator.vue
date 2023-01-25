f9bff8203468eb19018b06c10f1fdbbf0111c450
raj <raj@crackerdigital.com>
2023-01-25 13:37:15 +0530

    added calculator feature

---
+++ resources/assets/js/views/settings/Calculator.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="calculator-main-container">
+ <div class="container mt-5 pt-5">
+ <vue-advanced-calculator description="Advance Cacluclator" title="OmtBiz Cacluclator"/>
+ </div>
+  </div>
+</template>
+<script>
+import {VueAdvancedCalculator} from 'vue-advanced-calculator'
+import 'vue-advanced-calculator/dist/vue-advanced-calculator.min.css'
+export default {
+components: {
+  VueAdvancedCalculator
+},
+  data () {
+    return {
+    
+    }
+  },
+  mounted() {
+    document.getElementById('showModal').style.display="block";
+  },
+  methods: {
+   closeModal() {
+    document.getElementById('showModal').style.display="none";
+   }
+}
+}
+</script>
