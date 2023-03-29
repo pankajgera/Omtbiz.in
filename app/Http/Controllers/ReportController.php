4545a79f29fbc302aac93dc9f87278379f8e21fe
vipinkundal <vipinkundal15@gmail.com>
2023-03-29 12:36:58 +0530

    Customer report update

--- app/Http/Controllers/ReportController.php
+++ app/Http/Controllers/ReportController.php
@@ -343,11 +343,12 @@ class ReportController extends Controller
         $sum_opening_current_dr = 0;
         // --- 1. Calculate opening balance
         //Total opening balance include previous dates
-        if ('Cr' === $master->type) {
+        if ('Dr' === $master->type) {
             $total_opening_balance_cr = $total_opening_balance_cr + $master_opening_balance;
         } else {
             $total_opening_balance_dr = $total_opening_balance_dr + $master_opening_balance;
         }
+
         //For dates, add/sub total_opening so we get only single opening
         if ($total_opening_balance_cr > $total_opening_balance_dr) {
             $total_opening_balance_cr = abs($total_opening_balance_cr - $total_opening_balance_dr);
@@ -355,16 +356,15 @@ class ReportController extends Controller
         } else if ($total_opening_balance_cr < $total_opening_balance_dr) {
             $total_opening_balance_dr = abs($total_opening_balance_cr - $total_opening_balance_dr);
             $total_opening_balance_cr = 0;
-        } else if ($total_opening_balance_cr === $total_opening_balance_dr) {
+        } else {
             $total_opening_balance_cr = 0;
             $total_opening_balance_dr = 0;
         }
 
         // --- 2. Calculate current balance
         //Adding opening and current balance with total opening
-        //Because opening_dr/cr won't switch so we add it to current_cr/dr
-        $sum_opening_current_cr = $total_opening_balance_dr + $current_balance_cr;
-        $sum_opening_current_dr = $total_opening_balance_cr + $current_balance_dr;
+        $sum_opening_current_cr = $total_opening_balance_cr + $current_balance_cr;
+        $sum_opening_current_dr = $total_opening_balance_dr + $current_balance_dr;
 
         // --- 3. Calculate closing balance
         if ($sum_opening_current_cr > $sum_opening_current_dr) {
@@ -373,7 +373,7 @@ class ReportController extends Controller
         } else if ($sum_opening_current_cr < $sum_opening_current_dr) {
             $sum = $sum_opening_current_dr - $sum_opening_current_cr;
             $closing_balance_dr = abs($sum);
-        } else if ($sum_opening_current_cr === $sum_opening_current_dr) {
+        } else {
             $closing_balance_cr = $total_opening_balance_cr;
             $closing_balance_dr = $total_opening_balance_dr;
         }
