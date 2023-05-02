0018ceb6e76400e7285e1098240ac25c8810e48a
rajcracker <raj@crackerdigital.com>
2023-05-02 13:16:05 +0530

    update number with seprated comma

--- resources/assets/js/views/payments/Index.vue
+++ resources/assets/js/views/payments/Index.vue
@@ -164,7 +164,11 @@
           :label="$t('payments.amount')"
           width="20%"
           show="amount"
-        />
+        >
+        <template slot-scope="row">
+            ₹ {{ numberWithCommas(row.amount) }}
+          </template>
+          </table-column>
         <table-column
           :label="$t('payments.date')"
           sort-as="payment_date"
@@ -221,12 +225,14 @@ import { mapActions, mapGetters } from 'vuex'
 import MoonWalkerIcon from '../../../js/components/icon/MoonwalkerIcon'
 import moment from 'moment'
 import BaseButton from '../../components/base/BaseButton'
+import GlobalMixin from '../../helpers/mixins.js';
 
 export default {
   components: {
     'moon-walker-icon': MoonWalkerIcon,
     BaseButton,
   },
+  mixins:[GlobalMixin],
   data () {
     return {
       showFilters: false,
