be7be2d0807cf261f18a68bce7953deec8190d1f
vipinkundal <vipinkundal15@gmail.com>
2026-07-15 15:08:12 +0530

    Migrate table cell rendering to Vue setup syntax

--- resources/js/components/base/base-table/components/TableCell.js
+++ resources/js/components/base/base-table/components/TableCell.js
@@ -1,25 +1,27 @@
-export default {
-  functional: true,
+import { h } from 'vue'
 
+export default {
   props: ['column', 'row', 'responsiveLabel'],
 
-  render (createElement, { props }) {
-    const data = {}
+  setup (props) {
+    return () => {
+      const cellProps = props.column.cellClass
+        ? { class: props.column.cellClass }
+        : {}
 
-    if (props.column.cellClass) {
-      data.class = props.column.cellClass
+      if (props.column.template) {
+        return h('td', cellProps, props.column.template(props.row.data))
+      }
+
+      const value = props.column.formatter(
+        props.row.getValue(props.column.show),
+        props.row.data
+      )
+
+      return h('td', cellProps, [
+        h('span', props.responsiveLabel),
+        h('span', { innerHTML: value === null || value === undefined ? '' : value })
+      ])
     }
-
-    if (props.column.template) {
-      return createElement('td', data, props.column.template(props.row.data))
-    }
-
-    data.domProps = {}
-    data.domProps.innerHTML = props.column.formatter(props.row.getValue(props.column.show), props.row.data)
-
-    return createElement('td', [
-      createElement('span', props.responsiveLabel),
-      createElement('span', data.domProps.innerHTML)
-    ])
   }
 }
